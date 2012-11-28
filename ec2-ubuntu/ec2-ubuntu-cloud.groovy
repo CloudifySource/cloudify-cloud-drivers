e820a78130c543387974f5266b7805a522e173fe
Tamir Korem <tamir@gigaspaces.com>
2012-11-28 16:50:46 +0200

    Added Copyright

--- ec2-ubuntu/ec2-ubuntu-cloud.groovy
+++ ec2-ubuntu/ec2-ubuntu-cloud.groovy
@@ -1,3 +1,18 @@
+/*******************************************************************************
+* Copyright (c) 2012 GigaSpaces Technologies Ltd. All rights reserved
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
 /***************
  * Cloud configuration file for the Amazon ec2 cloud. Uses the default jclouds-based cloud driver.
  * See org.cloudifysource.dsl.cloud.Cloud for more details.
